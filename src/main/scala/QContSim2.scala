622039b63b3e9ae519e249a9d29ab3ad2076ff5c
Guannan Wei <guannanwei@outlook.com>
2023-03-15 19:34:33 -0400

    rename

--- src/main/scala/QContSim2.scala
+++ src/main/scala/QContSim2.scala
@@ -12,29 +12,27 @@ object QuantumEvalCPS {
 
   val summary: HashMap[Vector[Boolean], Double] = HashMap()
 
-  def evalGate(g: Gate, v: State, k: State => Ans): Ans = {
-    val State(d, bs) = v
+  def evalGate(g: Gate, s: State, k: State => Ans): Ans =
     g match {
-      case CCX(x, y, z) if isSet(bs, x) && isSet(bs, y) => k(State(d, neg(bs, z)))
-      case CCX(x, y, z) => k(v)
-      case H(x) if isSet(bs, x) =>
-        k(State(hscale * d, neg(bs, x)))
-        k(State(-1.0 * hscale * d, bs))
+      case CCX(x, y, z) if isSet(s.bs, x) && isSet(s.bs, y) => k(State(s.d, neg(s.bs, z)))
+      case CCX(x, y, z) => k(s)
+      case H(x) if isSet(s.bs, x) =>
+        k(State(hscale * s.d, neg(s.bs, x)))
+        k(State(-1.0 * hscale * s.d, s.bs))
       case H(x) =>
-        k(State(hscale * d, neg(bs, x)))
-        k(State(hscale * d, bs))
+        k(State(hscale * s.d, neg(s.bs, x)))
+        k(State(hscale * s.d, s.bs))
     }
-  }
 
-  def evalCircuit(c: Circuit, v: State, k: State => Ans): Ans =
-    if (c.isEmpty) k(v) else evalGate(c.head, v, s => evalCircuit(c.tail, s, k))
+  def evalCircuit(c: Circuit, s: State, k: State => Ans): Ans =
+    if (c.isEmpty) k(s) else evalGate(c.head, s, s => evalCircuit(c.tail, s, k))
 
-  def runCircuit(c: Circuit, v: State): Ans = {
+  def runCircuit(c: Circuit, s: State): Ans = {
     summary.clear
-    evalCircuit(c, v, summarize)
+    evalCircuit(c, s, summarize)
   }
 
-  def summarize(v: State): Unit =
-    if (summary.contains(v.bs)) summary(v.bs) = summary(v.bs) + v.d
-    else summary(v.bs) = v.d
+  def summarize(s: State): Unit =
+    if (summary.contains(s.bs)) summary(s.bs) = summary(s.bs) + s.d
+    else summary(s.bs) = s.d
 }
