0bc771fe88d989503ff85e54fc66d6525e33b88c
Guannan Wei <guannanwei@outlook.com>
2023-05-07 01:41:53 -0400

    simulation by linear algebra

--- src/main/scala/Schrodinger.scala
+++ src/main/scala/Schrodinger.scala
@@ -16,30 +16,131 @@ object Complex {
 
 import Complex._
 
-case class Matrix(m: Array[Array[Complex]]) {
-  def ⊗(m1: Matrix): Matrix = ???
-  def *(m1: Matrix): Matrix = ???
-  def *(m1: Array[Complex]): Array[Complex] = ???
+object Matrix {
+  type Matrix = Array[Array[Complex]]
+
+  def identity(n: Int): Matrix = {
+    val result = zeros(n)
+    for (i <- 0 until n) {
+      result(i)(i) = 1
+    }
+    result
+  }
+  def zeros(n: Int): Matrix = {
+    val result = Array.ofDim[Complex](n, n)
+    for (i <- 0 until n) {
+      for (j <- 0 until n) {
+        result(i)(j) = 0
+      }
+    }
+    result
+  }
+  def zerosVec(n: Int): Array[Complex] = {
+    val result = Array.ofDim[Complex](n)
+    for (i <- 0 until n) result(i) = 0
+    result
+  }
+
+  def prettyPrint(A: Matrix): String = {
+    val sb = new StringBuilder
+    val nRows = A.size
+    val nCols = A(0).size
+    sb ++= "["
+    for (i <- 0 until nRows) {
+      sb ++= (new ArrayOps(A(i))).toString
+      sb ++= "\n"
+    }
+    sb ++= "]\n"
+    sb.toString
+  }
+
+  implicit class ArrayOps(A: Array[Complex]) {
+    override def toString: String = {
+      val sb = new StringBuilder
+      sb ++= "["
+      for (i <- 0 until A.size) sb ++= s"${A(i)}, "
+      sb ++= "]"
+      sb.toString
+    }
+  }
+
+  implicit class MatrixOps(A: Matrix) {
+    def pPrint: String = prettyPrint(A)
+    // Kronecker product
+    def ⊗(B: Matrix): Matrix = {
+      val nRowsA = A.size
+      val nColsA = A(0).size
+      val nRowsB = B.size
+      val nColsB = B(0).size
+      val result = Array.ofDim[Complex](nRowsA * nRowsB, nColsA * nColsB)
+      for (i <- 0 until nRowsA; j <- 0 until nColsA) {
+        for (k <- 0 until nRowsB; l <- 0 until nColsB) {
+          result(i * nRowsB + k)(j * nColsB + l) = A(i)(j) * B(k)(l)
+        }
+      }
+      result
+    }
+    // Matrix multiplication
+    def *(B: Matrix): Matrix = {
+      val nRowsA = A.size
+      val nColsA = A(0).size
+      val nRowsB = B.size
+      val nColsB = B(0).size
+      require(nColsA == nRowsB, s"dimension error nColsA=$nColsA, nRowsB=$nRowsB \n${prettyPrint(A)} * ${prettyPrint(B)}")
+      val result = Array.ofDim[Complex](nRowsA, nColsB)
+      for (i <- 0 until nRowsA) {
+        for (j <- 0 until nColsB) {
+          var sum: Complex = 0.0
+          for (k <- 0 until nColsA) {
+            sum += A(i)(k) * B(k)(j)
+          }
+          result(i)(j) = sum
+        }
+      }
+      result
+    }
+  }
+}
+
+import Matrix._
+
+case class Gate(id: String, m: Matrix) {
   def arity: Int = pow(m.size, 0.5).toInt
 }
-
-object Matrix {
-  def identity(n: Int): Matrix = ???
-  def zeros(n: Int): Matrix = ???
+object Gate {
+  val isq2 = 1.0 / pow(2.0, 0.5) 
+  val H = Gate("H",
+    Array(
+      Array(isq2, isq2),
+      Array(isq2, -isq2)
+    ))
+  val CNOT = Gate("CNOT", 
+    Array(
+      Array(1, 0, 0, 0),
+      Array(0, 1, 0, 0),
+      Array(0, 0, 0, 1),
+      Array(0, 0, 1, 0),
+    ))
 }
 
-class QState(var s: Array[Complex], size: Int) {
-  def op(g: Matrix, i: Int) = {
-    val iLeft = Matrix.identity(pow(i, 2).toInt)
-    val iRight = Matrix.identity(size - i - g.arity)
-    s = ((iLeft ⊗ g) ⊗ iRight) * s
+class QState(var state: Matrix, size: Int) {
+  def op(g: Gate, i: Int) = {
+    //println(pow(2, i).toInt)
+    val iLeft = Matrix.identity(pow(2, i).toInt)
+    //println(iLeft.pPrint)
+    //println(pow(2, size - i - g.arity).toInt)
+    val iRight = Matrix.identity(pow(2, size - i - g.arity).toInt)
+    //println(iRight.pPrint)
+    state = ((iLeft ⊗ g.m) ⊗ iRight) * state
   }
+  def H(i: Int): Unit = op(Gate.H, i)
+  def CNOT(i: Int): Unit = op(Gate.CNOT, i)
 }
 
 object QState {
   def apply(n: Int): QState = {
-    val s = new Array[Complex](pow(n, 2).toInt)
+    val s = Matrix.zerosVec(pow(n, 2).toInt)
     s(0) = 1
-    new QState(s, n)
+    new QState(s.map(Array(_)), n)
   }
 }
---
+++ src/test/scala/MatrixTest.scala
@@ -0,0 +1,89 @@
+package quantum.tensor
+
+import math.pow
+import org.scalatest.funsuite.AnyFunSuite
+
+import Matrix._
+
+class SchrodingerTest extends AnyFunSuite {
+  test("EPR") {
+    val s = QState(2)
+    s.H(0)
+    s.CNOT(0)
+    assert(s.state.flatten.toList == List[Complex](Gate.isq2, 0, 0, Gate.isq2))
+  }
+}
+
+class MatrixTest extends AnyFunSuite {
+
+  test("Identity") {
+    val m1: Matrix = Array(
+      Array(1, 0, 0),
+      Array(0, 1, 0),
+      Array(0, 0, 1)
+    )
+    assert(Matrix.identity(3).flatten.toList == m1.flatten.toList)
+  }
+
+  test("MatMul") {
+    val m1: Matrix = Array(
+      Array(1, 0, 1),
+      Array(2, 1, 1),
+      Array(0, 1, 1),
+      Array(1, 1, 2)
+    )
+    val m2: Matrix = Array(
+      Array(1, 2, 1),
+      Array(2, 3, 1),
+      Array(4, 2, 2)
+    )
+    val m3: Matrix = Array(
+      Array(5, 4, 3),
+      Array(8, 9, 5),
+      Array(6, 5, 3),
+      Array(11, 9, 6)
+    )
+    assert((m1 * m2).flatten.toList == m3.flatten.toList)
+  }
+
+  test("Kronecker") {
+    val m1: Matrix = Array(
+      Array(1, 2),
+      Array(3, 4)
+    )
+    val m2: Matrix = Array(
+      Array(0, 5),
+      Array(6, 7)
+    )
+    val m3: Matrix = Array(
+      Array(0, 5, 0, 10),
+      Array(6, 7, 12, 14),
+      Array(0, 15, 0, 20),
+      Array(18, 21, 24, 28)
+    )
+
+    assert((m1 ⊗ m2).flatten.toList == m3.flatten.toList)
+
+    val m4: Matrix = Array(
+      Array(1, -4, 7),
+      Array(-2, 3, 3)
+    )
+    val m5: Matrix = Array(
+      Array(8, -9, -6, 5),
+      Array(1, -3, -4, 7),
+      Array(2, 8, -8, -3),
+      Array(1, 2, -5, -1)
+    )
+    val m6: Matrix = Array(
+      Array(8, -9, -6, 5, -32, 36, 24, -20, 56, -63, -42, 35),
+      Array(1, -3, -4, 7, -4, 12, 16, -28, 7, -21, -28, 49),
+      Array(2, 8, -8, -3, -8, -32, 32, 12, 14, 56, -56, -21),
+      Array(1, 2, -5, -1, -4, -8, 20, 4, 7, 14, -35, -7),
+      Array(-16, 18, 12, -10, 24, -27, -18, 15, 24, -27, -18, 15),
+      Array(-2, 6, 8, -14, 3, -9, -12, 21, 3, -9, -12, 21),
+      Array(-4, -16, 16, 6, 6, 24, -24, -9, 6, 24, -24, -9),
+      Array(-2, -4, 10, 2, 3, 6, -15, -3, 3, 6, -15, -3)
+    )
+    assert((m4 ⊗ m5).flatten.toList == m6.flatten.toList)
+  }
+}
