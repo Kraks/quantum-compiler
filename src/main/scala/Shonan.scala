96fc45404133ea68f76ea636fdc218a84983551d
Guannan Wei <guannanwei@outlook.com>
2023-05-09 01:29:27 -0400

    staged matrix-vector product

---
+++ src/main/scala/Shonan.scala
@@ -0,0 +1,54 @@
+package shonan
+
+// The Shonan Challenge - matrix vector product
+// https://scala-lms.github.io/tutorials/shonan.html
+
+import lms.core._
+import lms.core.stub._
+import lms.core.virtualize
+import lms.macros.SourceContext
+import lms.thirdparty.CLibs
+import lms.thirdparty.CCodeGenLibs
+
+import lms.core.Backend._
+
+object Shonan {
+  val A = scala.Array
+  val a =
+    A(A(1, 1, 1, 1, 1), 
+      A(0, 0, 0, 0, 0), 
+      A(0, 0, 1, 0, 0), 
+      A(0, 0, 0, 0, 0),
+      A(0, 0, 1, 0, 1))
+
+  val snippet = new DslDriver[Array[Int], Array[Int]] {
+    def unrollIf(c: Boolean, r: Range) = new {
+      def foreach(f: Rep[Int] => Rep[Unit]) = {
+        if (c) for (j <- (r.start until r.end): Range) f(j)
+        else for (j <- (r.start until r.end): Rep[Range]) f(j)
+      }
+    }
+
+    def snippet(v: Rep[Array[Int]]) = {
+      def matVecProd(a0: Array[Array[Int]], v: Rep[Array[Int]]): Rep[Array[Int]] = {
+        val n = a0.length
+        val a = staticData(a0)
+        val v1 = NewArray[Int](n)
+
+        for (i <- (0 until n): Range) {
+          val sparse = a0(i).count(_ != 0) < 3
+          for (j <- unrollIf(sparse, 0 until n)) {
+            v1(i) = v1(i) + a(i).apply(j) * v(j)
+          }
+        }
+        v1
+      }
+      matVecProd(a, v)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(snippet.code)
+    assert(snippet.eval(Array(1,2,3,4,5)).toList == List(15, 0, 3, 0, 8))
+  }
+}
